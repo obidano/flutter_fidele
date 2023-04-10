846771bd3378d91c6aabeae9c657218cf28ff167
obidano <omotetedan2@gmail.com>
2023-04-10 09:54:33 +0100

    feat: constantes

---
+++ lib/utils/Constantes.dart
@@ -0,0 +1,38 @@
+
+class Constantes{
+  static String BASE_URL="http://192.168.162.198/api_flutter";
+  static String fidelesEndpoint="/fideles.php";
+
+  static List<Map> fakeFideleList=[
+    {
+    "id": 1,
+    "prenom": "Stormie",
+    "nom": "MacAllen",
+    "age": 85,
+    "image": null
+  }, {
+    "id": 2,
+    "prenom": "Bobinette",
+    "nom": "Buyers",
+    "age": 28,
+    "image": "assets/businessman.png"
+  }, {
+    "id": 3,
+    "prenom": "Emiline",
+    "nom": "Chapier",
+    "age": 49,
+    "image": "assets/old-man.png"
+  }, {
+    "id": 4,
+    "prenom": "Annecorinne",
+    "nom": "Klehyn",
+    "age": 80,
+    "image": "assets/profile.png"
+  }, {
+    "id": 5,
+    "prenom": "Jesse",
+    "nom": "Cusiter",
+    "age": 87,
+    "image": "assets/programmer.png"
+  }];
+}
