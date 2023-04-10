beadeff32c2eb5fb84423dbcfe41f63db7cf0fa9
obidano <omotetedan2@gmail.com>
2023-04-10 12:37:36 +0100

    feat: envoie données par API Post

--- lib/controllers/FideleCtrl.dart
+++ lib/controllers/FideleCtrl.dart
@@ -1,30 +1,40 @@
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_fidele/utils/Constantes.dart';
 import 'package:http/http.dart' as http;
 import '../models/FideleModele.dart';
 
-class FideleCtrl with ChangeNotifier{
-  List<FideleModele> fideles=[];
-  bool loading=false;
+class FideleCtrl with ChangeNotifier {
+  List<FideleModele> fideles = [];
+  bool loading = false;
 
-  void recuperDataAPI() async{
-      var url=Uri.parse("${Constantes.BASE_URL}${Constantes.fidelesEndpoint}");
-      loading=true;
-      notifyListeners();
-      var reponse= await http.get(url);
-      print(reponse.runtimeType);
-      print(reponse.body.runtimeType);
-      print(reponse.body); // contient le resultat de la reponse
-      print(reponse.statusCode); // contient le code http
-      loading=false;
-      notifyListeners();
-
+  Future<bool> envoieDonneesAuth(Map data) async {
+    var url = Uri.parse("${Constantes.BASE_URL}${Constantes.authEndpoint}");
+    var dataStr = json.encode(data);
+    var reponse = await http.post(url, body: dataStr);
+    print(reponse.body);
+    if (reponse.statusCode == 200) {
+      return true;
+    }
+    return false;
   }
 
+  void recuperDataAPI() async {
+    var url = Uri.parse("${Constantes.BASE_URL}${Constantes.fidelesEndpoint}");
+    loading = true;
+    notifyListeners();
+    var reponse = await http.get(url);
+    print(reponse.runtimeType);
+    print(reponse.body.runtimeType);
+    print(reponse.body); // contient le resultat de la reponse
+    print(reponse.statusCode); // contient le code http
+    loading = false;
+    notifyListeners();
+  }
 }
 
-void main(){
-  var f= FideleCtrl();
+void main() {
+  var f = FideleCtrl();
   f.recuperDataAPI();
 }
