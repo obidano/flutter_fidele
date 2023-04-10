33e256a4ad09d32d6cfd1dae2f0ab8ba6b7945f5
obidano <omotetedan2@gmail.com>
2023-04-10 12:38:02 +0100

    feat: declaration du controlleur avec Provider

--- lib/apps/MonApplication.dart
+++ lib/apps/MonApplication.dart
@@ -2,13 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_fidele/pages/Demo.dart';
 import 'package:flutter_fidele/pages/FidelesPage.dart';
 import 'package:flutter_fidele/pages/LoginPage.dart';
+import 'package:provider/provider.dart';
+
+import '../controllers/FideleCtrl.dart';
+import '../pages/BrouillonPage.dart';
 
 class MonApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FidelesPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_)=> FideleCtrl()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginPage(),
+      ),
     );
   }
 }
