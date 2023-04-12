71c9883144dcac38d8c45011598ebe0cac203bd7
obidano <omotetedan2@gmail.com>
2023-04-12 09:53:17 +0100

    feat: initialisation get_storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fidele/apps/MonApplication.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main(){
+void main() async{
+  await GetStorage.init();
   runApp(MonApplication());
 
 }
