819e2bd7f6b4adc54002f667100781b9a5c38899
obidano <omotetedan2@gmail.com>
2023-04-12 16:33:45 +0100

    feat: injection stockage dans fidele ctrl et stockage de la liste apres echec reseau

--- lib/apps/MonApplication.dart
+++ lib/apps/MonApplication.dart
@@ -9,6 +9,7 @@ import 'package:provider/provider.dart';
 import '../controllers/FideleCtrl.dart';
 import '../controllers/UserCtrl.dart';
 import '../pages/BrouillonPage.dart';
+import '../pages/FormFidelePage.dart';
 import '../utils/Stockage.dart';
 
 class MonApplication extends StatelessWidget {
@@ -19,7 +20,7 @@ class MonApplication extends StatelessWidget {
     var user=box.read<Map>(Stockage.userKey);
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_)=> FideleCtrl()),
+        ChangeNotifierProvider(create: (_)=> FideleCtrl(stockage: box)),
         ChangeNotifierProvider(create: (_)=> UserCtrl(stockage: box )),
         ChangeNotifierProvider(create: (_)=> EventCtrl()),
       ],
--- lib/controllers/FideleCtrl.dart
+++ lib/controllers/FideleCtrl.dart
@@ -3,13 +3,17 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_fidele/utils/Constantes.dart';
 import 'package:flutter_fidele/utils/requetes.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
 import '../models/FideleModel.dart';
+import '../utils/Stockage.dart';
 
 class FideleCtrl with ChangeNotifier {
   List<FideleModel> fideles = [];
   bool loading = false;
+  GetStorage? stockage;
 
+  FideleCtrl({this.stockage});
 
   void recuperDataAPI() async {
     var url = "${Constantes.BASE_URL}${Constantes.fidelesEndpoint}";
@@ -17,15 +21,24 @@ class FideleCtrl with ChangeNotifier {
     loading = true;
     notifyListeners();
     var reponse = await getData(url);
+    print(reponse);
     if(reponse!=null){
       List<FideleModel> temp= reponse
           .map<FideleModel>((e) => FideleModel.fromJson(e)).toList();
       fideles=temp;
+      stockage?.write(Stockage.fidelesKey, reponse);
       notifyListeners();
       /*for(var r in reponse){
         var r_temp=FideleModel.fromJson(r);
         temp.add(r_temp);
       }*/
+    }else{
+      var dataStockee= stockage?.read(Stockage.fidelesKey);
+      var temp= dataStockee.map<FideleModel>(
+              (e)=> FideleModel.fromJson(e) ).toList();
+      fideles=temp;
+      notifyListeners();
+      print("data stockee:  $temp");
     }
     loading = false;
     notifyListeners();
