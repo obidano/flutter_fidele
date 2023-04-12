0b0156f28dfcfb80a91adb785cd644942c2c8a73
obidano <omotetedan2@gmail.com>
2023-04-12 09:53:47 +0100

    feat: fideles page synchronisation avec le controlleur

--- lib/pages/FidelesPage.dart
+++ lib/pages/FidelesPage.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_fidele/models/FideleModele.dart';
+import 'package:flutter_fidele/controllers/UserCtrl.dart';
+import 'package:flutter_fidele/models/FideleModel.dart';
+import 'package:flutter_fidele/pages/LoginPage.dart';
+import 'package:flutter_fidele/utils/Stockage.dart';
+import 'package:provider/provider.dart';
 
+import '../controllers/FideleCtrl.dart';
 import '../utils/Constantes.dart';
 
 class FidelesPage extends StatefulWidget {
@@ -12,7 +17,21 @@ class FidelesPage extends StatefulWidget {
 
 class _FidelesPageState extends State<FidelesPage> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      var fideleCtrl = context.read<FideleCtrl>();
+      fideleCtrl.recuperDataAPI();
+
+    });
+
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // var fideleCtrl = context.watch<FideleCtrl>();
+    print("Recharge de la page");
     return Scaffold(
       appBar: _appBar(),
       body: _body(),
@@ -20,27 +39,59 @@ class _FidelesPageState extends State<FidelesPage> {
   }
 
   AppBar _appBar() {
+    var fideleCtrl = context.watch<FideleCtrl>();
+
     return AppBar(
-      title:
-          Text("Liste des Fideles (0)", style: TextStyle(color: Colors.black)),
+      title: Text("Liste des Fideles (${fideleCtrl.fideles.length})",
+          style: TextStyle(color: Colors.black)),
       backgroundColor: Colors.transparent,
       elevation: 0,
+      actions: [
+        IconButton(
+            onPressed: () {
+              setState(() {});
+              return;
+              var userCtrl = context.read<UserCtrl>();
+              userCtrl.stockage?.remove(Stockage.userKey);
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (_) => LoginPage()));
+            },
+            color: Colors.black,
+            icon: Icon(Icons.exit_to_app))
+      ],
     );
   }
 
   Widget _body() {
-    return ListView.builder(
-        itemCount: Constantes.fakeFideleList.length,
-        itemBuilder: (ctx, i) {
-          var fidele = Constantes.fakeFideleList[i];
-          var f = FideleModele.fromJson(fidele);
-          // Text("${f.nom}")
-          return ListTile(
-            title: Text("${f.nom}"),
-            subtitle: Text("${f.prenom}"),
-            trailing: Icon(Icons.arrow_right),
-            leading: f.image != null? Image.asset(f.image!) : Icon(Icons.error),
-          );
-        });
+    var userCtrl = context.watch<UserCtrl>();
+    var fideleCtrl = context.watch<FideleCtrl>();
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Text("Connecté en tant que: ${userCtrl.user?.username}"),
+        ),
+        Expanded(
+          child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: fideleCtrl.fideles.length,
+              itemBuilder: (ctx, i) {
+                var f = fideleCtrl.fideles[i];
+                //var f = FideleModel.fromJson(fidele);
+                // Text("${f.nom}")
+                return ListTile(
+                  title: Text("${f.nom}"),
+                  subtitle: Text("${f.prenom}"),
+                  trailing: Icon(Icons.arrow_right),
+                  leading: f.image != null
+                      ? Image.network("${Constantes.BASE_URL}/${f.image!}")
+                      : Icon(Icons.error),
+                );
+              }),
+        ),
+      ],
+    );
   }
 }
